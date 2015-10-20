1354c2845ff06d7b699c1455acecca2e11fcaa32
Kristján Patrekur <krillinn@gmail.com>
2015-10-20 19:36:31 +0200

    code fixing

--- java/java.js
+++ java/java.js
@@ -19,10 +19,10 @@ $(document).ready(function() {
 		totalPrice: 0,
 		drawProducts: function() {
 
-			// Drop the while table
+			// Drop the whole table
 			$('#cartcontent tbody').remove();
 
-			// Insert all the information to the tale
+			// Show the cart information
 			for(var i = 0; i < cart.products.length; i++){
 				if(cart.products[i].display === true) {
 					var row = $("<tr/>");
@@ -43,24 +43,19 @@ $(document).ready(function() {
 				cart.updateTotalPrice(cart.products[id].price * quant);
 				cart.products[id].quant += quant;
 				cart.products[id].display = true;
-				// Update total price and display it
+				// Draw changes
 				cart.drawProducts();
 				return true;
 			}
-
 			return false;
 		},
 		addProduct: function(id, quant) {
-
 			// Call the function to update the item status in the cart
 			// If false there is no item
 			if(cart.justUpdate(id, quant) === true) {
 				return;
 			}
-
-			// If the item does not exist
 			// TODO ERROR
-			// cart.products.push( new product(id, name, price, 1) );
 		},
 		deleteProducts: function(){
 			cart.products = [];
@@ -73,30 +68,13 @@ $(document).ready(function() {
 
 	var inisiateDatabase = function(){
 		/* Here we add make the "database" */
-		cart.products.push( new product("Balloon", 25) );
+		cart.products.push( new product("Balloon", 25, 1) );
 		cart.products.push( new product("Pizza", 50) );
 	}; inisiateDatabase();
-
-	// $( ".product" ).on( "click", function() {
-
-	// 	// Here we extract information from HTML (never done in practice)
-	// 	var id = $(this).find('p:eq(0)').text();
-	// 	var name = $(this).find('p:eq(1)').text();
-	// 	var price = $(this).find('p:eq(2)').text();
-	// 	cart.addProduct(id, name, parseFloat(price.split('$')[1]));
-	// });
-
-	// $( ".addProduct" ).submit(function(event) {
-	// 	var id = $( "button:first" ).val();
-	// 	var quant = $( "input:first" ).val();
-	// 	console.log(id, quant);
-	// 	event.preventDefault();
-	// });
 
 	$('.addProduct').on('submit', function () {
 		var id = $(this).find('button').val();
 		var quant = parseInt($(this).find('input[name="quantity"]').val());
-		console.log(id, quant);
 		cart.addProduct(id, quant);
 		return false;
 	});
@@ -113,17 +91,4 @@ $(document).ready(function() {
  //    });
 
 });
-
-
-
-// questions:
-// 1. is this something that i can use?
-// 2. Is there anything that you can tell me about my code? Is there a better way? 
-// 3. My idea about having the objects.
-
-
-
-
-
-
 
