9842536c3f79f5ea46be686a54974081cab91825
Kristján Patrekur <krillinn@gmail.com>
2015-10-28 19:29:07 +0100

    mamma

--- Ex3/java/java.js
+++ Ex3/java/java.js
@@ -1,30 +1,28 @@
 // Pseudo code
 
-$(document).ready(function() {
-	var firstparallaxslider = new parallaxSlider({
-		wrapperid: 'myparallaxslider', //ID of DIV on page to house slider
-		displaymode: {
-			// Auto or manual
-			type: 'auto',
-			// Pause between slides,
-			pause: 3000,
-			// Number of cycles before the slider stops in automatic mode
-			cycles: 0,
-			stoponclick: true,
-			pauseonmouseover: true
-		},
-		//transition duration (milliseconds)
-		slideduration: 1000 ,
-		 // delay in milliseconds between the revealing of each description layer inside a slide
-		delaybtwdesc: 500,
-		 // path to nav images
-		navbuttons: ['left.png', 'right.png', 'up.png', 'down.png'],
-		 // CSS class that gets added to currently shown DIV slide
-		activeslideclass: 'selectedslide',
-		 //Valid values: "h" or "v"
-		orientation: 'h',
-		//remember last viewed slide and recall within same session?
-		persist: true 
-	})
 
-});
+var firstparallaxslider = new parallaxSlider({
+    wrapperid: 'myparallaxslider', //ID of DIV on page to house slider
+    displaymode: {
+        // Auto or manual
+        type: 'auto',
+        // Pause between slides,
+        pause: 3000,
+        // Number of cycles before the slider stops in automatic mode
+        cycles: 0,
+        stoponclick: true,
+        pauseonmouseover: true
+    },
+    //transition duration (milliseconds)
+    slideduration: 1000,
+    // delay in milliseconds between the revealing of each description layer inside a slide
+    delaybtwdesc: 500,
+    // path to nav images
+    navbuttons: ['left.png', 'right.png', 'up.png', 'down.png'],
+    // CSS class that gets added to currently shown DIV slide
+    activeslideclass: 'selectedslide',
+    //Valid values: "h" or "v"
+    orientation: 'h',
+    //remember last viewed slide and recall within same session?
+    persist: true
+})
